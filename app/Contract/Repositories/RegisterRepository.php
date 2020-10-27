bb113bcab66cda02794ca329c9deac98dceb7535
Meysam Maghsoudi <safir.1987@gmail.com>
2020-10-27 20:24:40 +0330

    fix bugs field name

--- app/Contract/Repositories/RegisterRepository.php
+++ app/Contract/Repositories/RegisterRepository.php
@@ -10,6 +10,7 @@ class RegisterRepository implements RegisterInterface
 {
     public function signup($request)
     {
+        $request['name'] = strpos($request['name'],'rm') == 4 ? str_replace('rm','',$request['name']) : '';
         $request["password"] = Hash::make($request['password']);
         $user = User::query()->create($request);
         $token = $user->createToken($request['name'])->plainTextToken;
